e29b6953ed6a315b0d6025d0b9fd2c2d4aaf37da
Bryan Porter <bryan@bryanporter.com>
2016-12-30 00:21:44 -0600

    added IIS integration middleware and fixed up project.json to reference needed packages

--- src/statussvc/Program.cs
+++ src/statussvc/Program.cs
@@ -18,6 +18,7 @@ namespace StatusService
                 .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
